578377f79e9aeb4ed9ec248a5ab382da01c9df18
Liamkim <liamkim1018@gmail.com>
2025-10-31 18:44:12 +0900

    chore(setup): ktlint 적용 및 테스트 의존성 추가 (#1)

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,4 +7,12 @@ plugins {
 dependencies {
     implementation(project(":modules:auth"))
     implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.webflux)
+
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.kotest.extensions.spring)
+    testImplementation(libs.kotest.runner.junit5)
+    testImplementation(libs.kotest.assertions.core)
+    testImplementation(libs.kotest.framework.engine)
+    testImplementation(libs.mockk)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,19 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.spring.boot) apply false
     alias(libs.plugins.spring.dependency.management) apply false
+    alias(libs.plugins.ktlint) apply false
 }
 
 allprojects {
     repositories {
         mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
     }
 }
