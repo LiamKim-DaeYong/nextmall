6e0f73ce43eb1675f6d834cefdecf00ad9825c7f
DaeYong Kim <liamkim1018@gmail.com>
2025-11-09 20:36:28 +0900

    feat(user): 회원 등록 유스케이스 테스트 추가 및 PasswordEncoder 변경 (#11)
    * feat(user): 회원 등록 유스케이스 테스트 추가 및 PasswordEncoder 변경
    
    - RegisterUserUseCaseTest 추가로 회원 등록 테스트 커버리지 확보
    - `BCryptPasswordEncoder`를 인터페이스 `PasswordEncoder`로 변경하여 유연성 확보
    - 테스트를 위한 Kotest 및 Mockk 의존성 추가
    
    * refactor(security): `BCryptPasswordEncoder`를 `PasswordEncoder` 인터페이스로 변경
    
    - 암호화 구현체의 유연성 확보를 위해 인터페이스 사용으로 변경.

--- common/security/src/main/kotlin/com/nextmall/common/security/SecurityConfig.kt
+++ common/security/src/main/kotlin/com/nextmall/common/security/SecurityConfig.kt
@@ -3,9 +3,10 @@ package com.nextmall.common.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 class SecurityConfig {
     @Bean
-    fun passwordEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
