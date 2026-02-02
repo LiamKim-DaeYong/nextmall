259d6e36565f7ecd728ef24556b1a79e1eec8287
Liamkim <liamkim1018@gmail.com>
2026-02-02 20:26:13 +0900

    fix(product): ProductViewClientResponse의 currency 필드 기본값 추가
    - currency 필드에 기본값(null) 설정하여 초기화 없이 사용할 수 있도록 수정

--- services/orchestrator-service/src/main/kotlin/com/nextmall/orchestrator/client/product/response/ProductViewClientResponse.kt
+++ services/orchestrator-service/src/main/kotlin/com/nextmall/orchestrator/client/product/response/ProductViewClientResponse.kt
@@ -6,7 +6,7 @@ data class ProductViewClientResponse(
     val id: Long,
     val name: String,
     val price: Money,
-    val currency: String,
+    val currency: String? = null,
     val stock: Int,
     val sellerId: Long,
     val category: String?,
